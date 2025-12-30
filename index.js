6cc36933eabdab095a15005da762d8d6d57340e4
Deepak S <deepak.github@outlook.com>
2025-12-30 15:34:04 +0530

    intial commit

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ const ReviewRoutes = require("./routes/ReviewRoutes");
 //middlewares
 
 app.use(
-  cors()
+  cors({
+    origin: [
+      "http://localhost:4000",
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ].filter(Boolean),
+    credentials: true,
+  })
 );
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
