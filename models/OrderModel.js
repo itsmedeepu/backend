61e1db40683605b2af7688899e853981ec4b7696
Deepak S <deepak.github@outlook.com>
2026-01-04 20:57:51 +0530

    fixed bugs

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -8,6 +8,7 @@ const orderSchema = new mongoose.Schema(
       {
         product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
         quantity: Number,
+        price: Number,
       },
     ],
     totalAmount: Number,
--- models/UserModel.js
+++ models/UserModel.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema(
     averageRating: { type: Number, default: 0 },
     ratingCount: { type: Number, default: 0 },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
 const UserModel = mongoose.model("User", userSchema);
